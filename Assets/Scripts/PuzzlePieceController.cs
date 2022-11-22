9aa5a48497ceaab8a32e01e181a0c6f27b4a715f
Jamie Myland <jamie.myland@gmail.com>
2022-11-22 15:34:06 +0000

    Created base 2D unity project

---
+++ Assets/Scripts/PuzzlePieceController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzlePieceController : MonoBehaviour
+{
+    private Color[] colours = new Color[6] {
+        Color.red,
+        Color.green,
+        Color.blue,
+        Color.cyan,
+        Color.magenta,
+        Color.yellow
+    };
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
